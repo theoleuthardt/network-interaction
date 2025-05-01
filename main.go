0727e3d9123f3b003004ad72002cbaa73ef11c71
theoleuthardt <theoleuthardt12@gmail.com>
2025-05-01 14:14:18 +0200

    feat: new wails frontend structure

--- main.go
+++ main.go
@@ -1,11 +1,129 @@
 package main
 
 import (
+	"embed"
+	"fmt"
+	"log"
 	"network-interaction/backend"
-	"network-interaction/frontend"
+	"network-interaction/utils"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/logger"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+var (
+	fastSend, dynamicSend, slowSend uint
 )
 
 func main() {
+	// Start backend
 	go backend.SetupServer()
-	frontend.SetupGUI()
+
+	go func() {
+		for {
+			fastSend = readUint("fast_send.txt")
+			dynamicSend = readUint("dynamic_send.txt")
+			slowSend = readUint("slow_send.txt")
+			time.Sleep(500 * time.Millisecond)
+		}
+	}()
+
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	icon := loadAppIcon()
+
+	err := wails.Run(&options.App{
+		Title:             "network-interaction",
+		Width:             1024,
+		Height:            768,
+		MinWidth:          1024,
+		MinHeight:         768,
+		MaxWidth:          1280,
+		MaxHeight:         800,
+		DisableResize:     false,
+		Fullscreen:        false,
+		Frameless:         false,
+		StartHidden:       false,
+		HideWindowOnClose: false,
+		BackgroundColour:  &options.RGBA{R: 255, G: 255, B: 255, A: 255},
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		Menu:             nil,
+		Logger:           nil,
+		LogLevel:         logger.DEBUG,
+		OnStartup:        app.startup,
+		OnDomReady:       app.domReady,
+		OnBeforeClose:    app.beforeClose,
+		OnShutdown:       app.shutdown,
+		WindowStartState: options.Normal,
+		Bind: []interface{}{
+			app,
+		},
+		// Windows platform specific options
+		Windows: &windows.Options{
+			WebviewIsTransparent: false,
+			WindowIsTranslucent:  false,
+			DisableWindowIcon:    false,
+			// DisableFramelessWindowDecorations: false,
+			WebviewUserDataPath: "",
+			ZoomFactor:          1.0,
+		},
+		// Mac platform specific options
+		Mac: &mac.Options{
+			TitleBar: &mac.TitleBar{
+				TitlebarAppearsTransparent: true,
+				HideTitle:                  false,
+				HideTitleBar:               false,
+				FullSizeContent:            false,
+				UseToolbar:                 false,
+				HideToolbarSeparator:       true,
+			},
+			Appearance:           mac.NSAppearanceNameDarkAqua,
+			WebviewIsTransparent: true,
+			WindowIsTranslucent:  true,
+			About: &mac.AboutInfo{
+				Title:   "network-interaction",
+				Message: "",
+				Icon:    icon,
+			},
+		},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func readUint(filename string) uint {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return 0
+	}
+	n, err := strconv.Atoi(string(data))
+	if err != nil {
+		return 0
+	}
+	utils.LogInfo(fmt.Sprintf("[FRONTEND] %s: %d", filename, n))
+	return uint(n)
+}
+
+func loadAppIcon() []byte {
+	icon, err := os.ReadFile("icon.png")
+	if err != nil {
+		log.Fatalf("Failed to read icon: %v", err)
+	}
+	return icon
 }
